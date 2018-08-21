7a0d114e9b4800e3086a50734d510be7e3289df3
faithoroone <faithorone59@gmail.com>
2018-08-21 18:27:33 +0300

    add post a question api #159869082

---
+++ API/question/models.py
@@ -0,0 +1,16 @@
+class Question():
+    def __init__(self, question_id, username, question):
+        self.question_id = 12345
+        self.username = username
+        self.question = question
+
+    def get_question_id(self):
+            return self.question_id
+
+    def get_username(self):
+            return self.username
+
+    def get_question(self):
+            return self.question
+
+questions =[]
