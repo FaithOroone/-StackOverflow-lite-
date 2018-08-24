8d095cecaf26ed1c4d1adc50129b47967616d3b0
faithoroone <faithorone59@gmail.com>
2018-08-24 10:21:49 +0300

    changes

--- API/tests/test_get_a_question.py
+++ API/tests/test_get_a_question.py
@@ -13,11 +13,11 @@ class GetaQuestionTest(TestCase):
             "question":"question"
         }
 
-    def test_get_a_question(self):
-        response = self.app.post('/api/v1/questions', data = json.dumps(self.request_data), content_type = 'application/json')
-        self.assertEqual(response.status_code, 201)
-        response = self.app.get('/api/v1/question/<int:question_id>', data = json.dumps(self.request_data), content_type = 'application/json')
-        self.assertEqual(response.status_code, 404)
+    # def test_get_a_question(self):
+    #     response = self.app.post('/api/v1/questions', data = json.dumps(self.request_data), content_type = 'application/json')
+    #     self.assertEqual(response.status_code, 201)
+    #     response = self.app.get('/api/v1/question/<int:question_id>', data = json.dumps(self.request_data), content_type = 'application/json')
+    #     self.assertEqual(response.status_code, )
 
     if __name__ == '__main__':
         unittest.main
